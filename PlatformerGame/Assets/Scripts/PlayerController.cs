4ca57890c0fdc3faea07a981d9969db18a428496
DmitriyShemet <borz.99@inbox.ru>
2022-10-29 15:19:49 +0200

    second

--- PlatformerGame/Assets/Scripts/PlayerController.cs
+++ PlatformerGame/Assets/Scripts/PlayerController.cs
@@ -9,25 +9,39 @@ public class PlayerController : MonoBehaviour
     float _moveHor;
     bool _isJumping;
     bool _isDashing;
+    Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         _moveHor = Input.GetAxisRaw("Horizontal") * _moveSpeed;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            _isJumping = true;
-        }
-        if (Input.GetButtonDown("Fire1"))
-        {
-            _isDashing = true;
-        }
-    }
+        if (Input.GetButtonDown("Jump")) _isJumping = true;
+
+        if (Input.GetButtonDown("Fire1")) _isDashing = true;
+    }   
 
     void FixedUpdate()
     {
         _controller.Move(_moveHor * Time.fixedDeltaTime, _isJumping, _isDashing);
         _isJumping = false;
         _isDashing = false;
+        AnimState();
+    }
+
+    void AnimState()
+    {
+        if(_moveHor != 0)
+        {
+            _anim.SetBool("Run", true);
+        }
+        else
+        {
+            _anim.SetBool("Run", false);
+        }
     }
 }
